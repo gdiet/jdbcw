cee52302034c5d93a4531bacdfad8f5789ba89f8
georg <gdiet@users.noreply.github.com>
2022-07-03 10:17:14 +0200

    basic project files

---
+++ src/test/java/gd/jdbcw/UseCase01Test.java
@@ -0,0 +1,10 @@
+package gd.jdbcw;
+
+import org.testng.annotations.Test;
+
+public class UseCase01Test {
+    @Test
+    public void run_ddl() {
+        System.out.println("Example test");
+    }
+}
